8c1170ffc9c896965cf13131c8baeed0fbba8f4d
MarkTBSS <marktbss@gmail.com>
2023-09-11 07:33:15 +0700

    [ADD] Java Files

---
+++ src/Solution.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        String mockInput = "42\n100\n125\n";
+        Scanner scanner = new Scanner(mockInput);
+        //Scanner scanner = new Scanner(System.in);
+        for (int i=0; i < 3; i++) { 
+            int a = scanner.nextInt();
+            System.out.println(a);
+        }
+        scanner.close();
+    }
+}
