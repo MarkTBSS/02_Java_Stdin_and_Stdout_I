02a048e25fa3599617e74f7b006ba58f33821173
MarkTBSS <marktbss@gmail.com>
2023-12-04 09:57:09 +0700

    [MOD] Delete .class

--- src/Solution.java
+++ src/Solution.java
@@ -1,14 +1,16 @@
 import java.util.Scanner;
 
 public class Solution {
+
     public static void main(String[] args) {
         String mockInput = "42\n100\n125\n";
         Scanner scanner = new Scanner(mockInput);
-        //Scanner scanner = new Scanner(System.in);
-        for (int i=0; i < 3; i++) { 
+        // Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < 3; i++) {
             int a = scanner.nextInt();
             System.out.println(a);
         }
         scanner.close();
     }
+
 }
